2066d9711217a6c8beac63970448e2b15d301b0e
sandii <chenzhibupt@qq.com>
2017-04-17 17:02:02 +0800

    update

---
+++ 3.linear-list/sequence-list.c
@@ -0,0 +1,50 @@
+/*
+* author: chenzhi <chenzhibupt@qq.com>
+* data: Apr 17, 2017
+*
+* sequence list
+*
+*/
+
+#include <stdio.h>
+
+#define OK    1
+#define ERROR 0
+#define TRUE  1
+#define FALSE 0
+
+#define MAXSIZE 20
+
+
+typedef int Status;
+typedef int ElemType;
+
+typedef struct {
+	ElemType data[MAXSIZE];
+	int length;
+} SqList; 
+
+
+SqList* init () {
+	SqList list;
+	SqList
+}
+
+Status insert () {}
+
+Status delete () {}
+
+ElemType getElem () {}
+
+int isEmpty () {}
+
+Status clear () {}
+
+int locateElem () {}
+
+int getLength () {}
+
+main () {
+
+}
+
