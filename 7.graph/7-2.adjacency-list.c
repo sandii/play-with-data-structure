973ca25f3fe95c36b66536abb053a06a1b7710d6
sandii <chenzhibupt@qq.com>
2017-05-30 22:16:44 +0800

    update

--- 7.graph/7-2.adjacency-list.c
+++ 7.graph/7-2.adjacency-list.c
@@ -1,26 +1,32 @@
 /*
 * author: chenzhi <chenzhibupt@qq.com>
-* data: May 22, 2017
+* data: May 30, 2017
 *
-* adjacent matrix
-* - i-j-weight: 0-4-6 means arc[0][4] = 6
+* adjacency list
 *
 */
 
 #include <stdio.h>
 #include <string.h>	// strlen strcpy
 #include <ctype.h>	// isdigit
-#include <stdlib.h> // malloc
+#include <stdlib.h> // malloc free
 
-#define VOID -1
 #define VMAX 20
 
+typedef struct ArcNode {
+	int adjacency;
+	int weight;
+	struct ArcNode* next;
+} ArcNode;
 typedef struct {
-	char vertex[VMAX];
-	int arc[VMAX][VMAX];
+	char data;
+	ArcNode* firstEdge;
+} VertexNode;
+typedef struct {
+	VertexNode vertex[VMAX];
 	int vertexNum;
 	int arcNum;
-} MGraph;
+} LGraph;
 
 
 void split (char* arr[], char* str, char gap) {
@@ -43,20 +49,25 @@ void split (char* arr[], char* str, char gap) {
 		}
 	}
 }
-void init (MGraph* gp, char* vStr, char* aStr) {
+void freeArr (char* arr[], int len) {
+	for (int i = 0; i < len; i++) {
+		free(arr[i]);
+	}
+}
+void init (LGraph* gp, char* vStr, char* aStr) {
 	// init vertexes
 	int vertexNum = 0;
 	for (char* cp = vStr; *cp != '\0'; cp++) {
-		gp -> vertex[vertexNum++] = *cp;		
+		gp -> vertex[vertexNum++].data = *cp;		
 	}
 	gp -> vertexNum = vertexNum;
 
-	// init arcs
+	// init arc list
 	for (int i = 0; i < vertexNum; i++) {
-		for (int j = 0; j < vertexNum; j++) {
-			gp -> arc[i][j] = i == j ? 0 : VOID;
-		}
+		gp -> vertex[i].firstEdge = NULL;
 	}
+
+	// get arcNum
 	int arcNum = 0;
 	for (char* cp = aStr; *cp != '\0'; cp++) {
 		if (*cp == ',') arcNum++;
@@ -64,7 +75,7 @@ void init (MGraph* gp, char* vStr, char* aStr) {
 	arcNum++;
 	gp -> arcNum = arcNum;
 
-	// fill arc matrix
+	// fill arc list
 	char* dotArr[arcNum];
 	char* numArr[3];
 	split(dotArr, aStr, ',');
@@ -72,22 +83,40 @@ void init (MGraph* gp, char* vStr, char* aStr) {
 		split(numArr, dotArr[k], '-');
 		int i = atoi(numArr[0]);
 		int j = atoi(numArr[1]);
-		gp -> arc[i][j] = atoi(numArr[2]);
+		ArcNode* temp = (ArcNode*) malloc(sizeof(ArcNode));
+		temp -> adjacency = j;
+		temp -> weight = atoi(numArr[2]);
+		temp -> next = gp -> vertex[i].firstEdge;
+		gp -> vertex[i].firstEdge = temp;
 	}
-
+	freeArr(dotArr, arcNum);
+	freeArr(numArr, 3);
 }
-void printV (MGraph* gp) {
+void clear (LGraph* gp) {
+	for (int i = 0; i < gp -> vertexNum; i++) {
+		while (gp -> vertex[i].firstEdge != NULL) {
+			ArcNode* temp = gp -> vertex[i].firstEdge;
+			gp -> vertex[i].firstEdge = temp -> next;
+			free(temp);
+		}
+	}
+	gp -> vertexNum = 0;
+	gp -> arcNum = 0;
+}
+void printV (LGraph* gp) {
 	printf("Vertex: ");
 	for (int i = 0; i < gp -> vertexNum; i++) {
-		printf("%c ", gp -> vertex[i]);
+		printf("%c ", gp -> vertex[i].data);
 	}
 	printf("\n\n");
 }
-void printA (MGraph* gp) {
+void printA (LGraph* gp) {
 	printf("Arc:\n");
 	for (int i = 0; i < gp -> vertexNum; i++) {
-		for (int j = 0; j < gp -> vertexNum; j++) {
-			printf("%2d ", gp -> arc[i][j]);
+		printf("%c - ", gp -> vertex[i].data);
+		for (ArcNode* ap = gp -> vertex[i].firstEdge; ap != NULL; ap = ap -> next) {
+			int j = ap -> adjacency;
+			printf("%c ", gp -> vertex[j].data);
 		}
 		printf("\n");
 	}
@@ -100,17 +129,17 @@ typedef int boolean;
 #define FALSE 0
 
 boolean visited[VMAX];
-void visitDFS (MGraph* gp, int i) {
-	printf("%c ", gp -> vertex[i]);
+void visitDFS (LGraph* gp, int i) {
+	printf("%c ", gp -> vertex[i].data);
 	visited[i] = TRUE;
 
-	for (int j = 0; j < gp -> vertexNum; j++) {
-		if (gp -> arc[i][j] <= 0) continue;
+	for (ArcNode* ap = gp -> vertex[i].firstEdge; ap != NULL; ap = ap -> next) {
+		int j = ap -> adjacency;
 		if (visited[j] == TRUE) continue;
 		visitDFS(gp, j);
 	}
 }
-void traverseDFS (MGraph* gp) {
+void traverseDFS (LGraph* gp) {
 	// initiate visited array
 	printf("Depth-first traverse: ");
 	for (int i = 0; i < gp -> vertexNum; i++) {
@@ -133,7 +162,7 @@ typedef struct {
 // set queue as a global variable
 Queue queue;
 
-void init () {
+void initQueue () {
 	queue.head = 0;
 	queue.tail = 0;
 	queue.len = 0;
@@ -151,16 +180,17 @@ int deQueue () {
 	int n = queue.data[queue.head];
 	queue.head = (queue.head == VMAX - 1) ? 0 : queue.head + 1;
 	(queue.len)--;
+	return n;
 }
 
 // bfs begins
-void visitBFS (MGraph* gp, int i) {
-	printf("%c ", gp -> vertex[i]);
+void visitBFS (LGraph* gp, int i) {
+	printf("%c ", gp -> vertex[i].data);
 	visited[i] = TRUE;
 
 	enQueue(i);
 }
-void traverseBFS (MGraph* gp) {
+void traverseBFS (LGraph* gp) {
 	printf("Breadth-first traverse: ");
 	for (int i = 0; i < gp -> vertexNum; i++) {
 		visited[i] = FALSE;
@@ -170,8 +200,8 @@ void traverseBFS (MGraph* gp) {
 		visitBFS(gp, k);
 		while (!emptyQueue()) {
 			int i = deQueue();
-			for (int j = 0; j < gp -> vertexNum; j++) {
-				if (gp -> arc[i][j] <= 0) continue;
+			for (ArcNode* ap = gp -> vertex[i].firstEdge; ap != NULL; ap = ap -> next) {
+				int j = ap -> adjacency;
 				if (visited[j] == TRUE) continue;
 				visitBFS(gp, j);
 			}
@@ -182,22 +212,25 @@ void traverseBFS (MGraph* gp) {
 
 
 main () {
-	MGraph gragh;
+	LGraph gragh;
 	// a simple graph, see 7-1-1.jpg
 	init(&gragh, (char*)("abcde"), (char*)("0-4-6,1-0-9,1-2-3,2-0-2,2-3-5,3-4-1"));
 	printV(&gragh);
 	printA(&gragh);
 	traverseDFS(&gragh);
+	initQueue();
 	traverseBFS(&gragh);
 	printf("\n\n");
 
 	// a more complicated graph, see 7-1-2.jpg
 	// ABCDEFGHI
 	// 012345678
+	clear(&gragh);
 	init(&gragh, (char*)("ABCDEFGHI"), (char*)("0-1-1,0-5-1,1-0-1,1-2-1,1-6-1,1-8-1,2-1-1,2-3-1,2-8-1,3-2-1,3-4-1,3-6-1,3-7-1,3-8-1,4-3-1,4-5-1,4-7-1,5-0-1,5-4-1,5-6-1,6-1-1,6-3-1,6-5-1,7-3-1,7-4-1,7-6-1,8-1-1,8-2-1,8-3-1"));
 	printV(&gragh);
 	printA(&gragh);
 	traverseDFS(&gragh);
+	initQueue();
 	traverseBFS(&gragh);
 }
 
